2880a24056f6dd7cd13ccc3bd89db6d0cd2b962a
Naing Lu Tun <nainglu7924@gmail.com>
2024-09-08 03:30:00 +0630

    successfully implemented the responsive design for tablets and moblie devices

--- src/Body.tsx
+++ src/Body.tsx
@@ -13,9 +13,11 @@ const Body = () => {
   return (
     <>
         <Header />
-        <div className="p-4 mt-[85px] flex-col mainContent">
-            <div className="currentContionAndFiveDaysForecastContainer">
-                <div className='bg-slate-200 rounded-xl pr-8 pl-8 pt-4 pb-4 mb-4 currentConditionContainer'>
+        {/* Marked */}
+        <div className="p-4 mt-[85px] flex-col md:flex md:flex-row md:gap-5 mainContent">
+            
+            <div className="md:w-[38%] md:fixed md:top-18  currentContionAndFiveDaysForecastContainer"> 
+                <div className='bg-slate-200 rounded-xl pr-4 pl-4 pt-4 pb-4 mb-4 currentConditionContainer'>
 
                     <h2 className='text-xl font-medium currentConditionHeader'>Now</h2>
 
@@ -33,6 +35,8 @@ const Body = () => {
                     <div className="dateAndLocationContainer">
                         <div className="flex gap-2 dateContainer">
                             <img src={blackCalender} alt="calender icon" className="w-6" />
+
+                            {/* make the day display dynamically either by api or man */}
                             <p className="text-[20px] opacity-80 dateText">Thursday 5, Sept</p>
                         </div>
 
@@ -43,16 +47,19 @@ const Body = () => {
                     </div>
                 </div>
 
-                <div className="bg-slate-200 rounded-xl pr-8 pl-8 pt-4 pb-4 mb-4 fiveDaysForecastContainer">
+                <div className="bg-slate-200 rounded-xl pr-4 pl-4 pt-4 pb-4 mb-4 fiveDaysForecastContainer">
                     <p className="text-xl font-medium mb-4 fiveDaysForecastHeader">5 days forecast</p>
 
                     {fiveDaysArr.map((_, index) => (
-                        <div key={index} className="flex items-center justify-between mt-3 mb-3">
+                        <div key={index} className="flex items-center justify-between mt-5 mb-5">
                             <div className=" flex gap-2 degreeIconAndDegreeContainer">
                                 {/* Replace with icons provided from open weather map api */}
                                 <img src={moonIcon} alt="degree icon" className="w-8"/>
                                 <p className="text-[20px] font-medium forecastDegree">25°</p>
                             </div>
+
+
+                            {/* make the days display dynamically either by api or man */}
 
                             <p className="text-base opacity-80 forcastDate">2 Sept</p>
 
@@ -61,13 +68,14 @@ const Body = () => {
                     ))}
                 </div>
             </div>
-
-            <div className="todayHighlightsAndTodayTimesContainer">
-                <div className="bg-slate-200 rounded-xl pr-8 pl-8 pt-4 pb-4 mb-4 todayHighlightsContainer">
+            
+            {/* Marked */}
+            <div className="md:w-[58%] md:ml-[42%] todayHighlightsAndTodayTimesContainer">
+                <div className="bg-slate-200 rounded-xl pr-4 pl-4 pt-4 pb-4 mb-4 todayHighlightsContainer">
 
                     <h2 className="text-xl font-medium mb-4 todayHighlightsHeader">Today Highlights</h2>
 
-                    <div className="bg-slate-300 rounded-xl pr-8 pl-8 pt-4 pb-4 mb-4 airQualityIndexContainer">
+                    <div className="bg-slate-300 rounded-xl pr-4 pl-4 pt-4 pb-4 mb-4 airQualityIndexContainer">
 
                         <h3 className="text-[20px] opacity-80 mb-6 airQualityIndexHeader">
                             Air Quality Index 
@@ -76,124 +84,153 @@ const Body = () => {
                                 </span>
                         </h3>
 
-                        <div className="flex w-[100%] justify-between airQualityContentContainer">
-                            <img src={windyIcon} alt="air quality icon" className="w-8 airQualityIcon" />
+                        <div className="flex w-[100%] justify-between items-center md:gap-3 airQualityContentContainer">
+                            <img src={windyIcon} alt="air quality icon" className="w-[40px] md:w-[50px] airQualityIcon" />
 
-                            <div className="grid grid-cols-2 airQualityInfoContainer">
-                                <p className="text-xl font-medium m-2 mt-0 PM25value">
-                                    117
-                                    <span className="text-[20px] opacity-80 ml-1 PM25">
-                                            PM25
+                            <div className="grid grid-cols-2 gap-4  md:flex md:gap-0 airQualityInfoContainer">
+
+                                <div className="flex items-center gap-1 md:flex-col-reverse md:mr-8 individualAirQualityInfoContainer">
+                                    <p className="text-xl font-medium PM25value">
+                                        117
+                                    </p>
+                                    
+                                    <span className="text-[20px] opacity-80 PM25">
+                                        PM25
                                     </span>
-                                </p>
+                                    
+                                </div>
 
-                                <p className="text-xl font-medium m-2 mt-0 SO2value">
-                                    29.8
-                                    <span className="text-[20px] opacity-80 ml-1 SO2">
+                                <div className="flex items-center gap-1 md:flex-col-reverse md:mr-8 individualAirQualityInfoContainer">
+                                    <p className="text-xl font-medium SO2value">
+                                        29.8
+                                        
+                                    </p>
+                                    <span className="text-[20px] opacity-80 SO2">
                                         SO2
                                     </span>
-                                </p>
-
-                                <p className="text-xl font-medium m-2 mt-0 NO2value">
+                                </div>
+                                
+                                <div className="flex items-center gap-1 md:flex-col-reverse md:mr-8 individualAirQualityInfoContainer">
+                                <p className="text-xl font-medium NO2value">
                                     43.2
-                                    <span className="text-[20px] opacity-80 ml-1 NO2">
-                                        NO2
-                                    </span>
                                 </p>
-
-                                <p className="text-xl font-medium m-2 mt-0 O3value">
-                                    0.150
-                                    <span className="text-[20px] opacity-80 ml-1 O3">
+                                
+                                <span className="text-[20px] opacity-80 NO2">
+                                        NO2
+                                </span>
+                                
+                                </div>
+                                
+                                <div className="flex items-center gap-1 md:flex-col-reverse individualAirQualityInfoContainer">
+                                    <p className="text-xl font-medium O3value">
+                                        0.150
+                                    </p>
+                                    <span className="text-[20px] opacity-80 O3">
                                         O3
                                     </span>
-                                </p>
+                                    
+                                </div>
+                                
                             </div>
                         </div>
                     </div>
 
-                    <div className="bg-slate-300 rounded-xl pr-8 pl-8 pt-4 pb-4 mb-4 sunriseAndSunSetContainer">
+                    <div className="bg-slate-300 rounded-xl pr-4 pl-4 pt-4 pb-4 mb-4 sunriseAndSunSetContainer">
                         <h3 className="text-[20px] opacity-80 mb-6 sunriseAndSunsetHeader" >Sunrise & Sunset</h3>
                         <div className="flex w-[100%] gap-10 sunriseAndSunsetContentContainer">
-                            <div className="sunriseContainer">
+                            <div className="flex flex-col md:flex-row md:items-center md:gap-4 sunriseContainer">
 
                                 {/* Replace with icon provided from api if possible */}
-                                <img src={sunIcon} alt="sun icon" className="w-8 sunriseIcon"/>
-                                <p className="text-base opacity-80 sunriseText">Sunrise</p>
+                                <img src={sunIcon} alt="sun icon" className="w-[40px] md:w-[50px] sunriseIcon"/>
+                                
+                                <div className="sunriseInfoContainer">
+                                    <p className="text-base opacity-80 sunriseText">Sunrise</p>
 
-                                {/* Replace with the time provided from the api */}
-                                <p className="text-xl font-medium sunriseTime">6:46 AM</p>
+                                    {/* Replace with the time provided from the api */}
+                                    <p className="text-xl font-medium sunriseTime">6:46 AM</p>
+                                </div>
+                                
                             </div>
 
-                            <div className="sunsetContainer">
+                            <div className="flex flex-col md:flex-row md:items-center md:gap-4 sunsetContainer">
                                 {/* Replace with icon provided from api if possible */}
-                                <img src={moonIcon} alt="moon icon" className="w-8 sunsetIcon"/>
-                                <p className="text-base opacity-80 sunriseText">Sunset</p>
+                                <img src={moonIcon} alt="moon icon" className="w-[40px] md:w-[50px] sunsetIcon"/>
 
-                                {/* Replace with the time provided from the api */}
-                                <p className="text-xl font-medium sunriseTime">6:21 pM</p>
+                                <div className="sunsetInfoContiner">
+                                    <p className="text-base opacity-80 sunsetText">Sunset</p>
+
+                                    {/* Replace with the time provided from the api */}
+                                    <p className="text-xl font-medium sunsetText">6:21 PM</p>
+                                </div>
+                                
                             </div>  
                         </div>    
                     </div>
 
-                    <div className="bg-slate-300 rounded-xl pr-8 pl-8 pt-4 pb-4 mb-4 humidityContainer">
 
-                        <h3 className="text-[20px] opacity-80 mb-6 humidityHeader">Humidity</h3>
+                    <div className="flex w-[100%] flex-col md:flex-row md:gap-4 humidityAndPressureContainer">
+                        <div className="bg-slate-300 rounded-xl pr-4 pl-4 pt-4 pb-4 mb-4 w-[100%] humidityContainer">
 
-                        <div className="flex w-[100%] justify-between humidityContentContainer">
-                            
-                            {/* Replace with icon provided from the api */}
-                            <img src={windyIcon} alt="humidity icon" className="w-8 humidityIcon" />
+                            <h3 className="text-[20px] opacity-80 mb-6 humidityHeader">Humidity</h3>
 
-                            {/* Replace with the humidity  provided from the api */}
-                            <p className="text-xl font-medium humidityPercent">73<span className="text-[20px] percent">%</span></p>
+                            <div className="flex w-[100%] justify-between humidityContentContainer">
+                                
+                                {/* Replace with icon provided from the api */}
+                                <img src={windyIcon} alt="humidity icon" className="w-8 humidityIcon" />
+
+                                {/* Replace with the humidity  provided from the api */}
+                                <p className="text-xl font-medium humidityPercent">73<span className="text-[20px] percent">%</span></p>
+                            </div>
+                        </div>
+
+                        <div className="bg-slate-300 rounded-xl pr-4 pl-4 pt-4 pb-4 mb-4 w-[100%] pressureContainer">
+
+                            <h3 className="text-[20px] opacity-80 mb-6 pressureHeader">Pressure</h3>
+
+                            <div className="flex w-[100%] justify-between pressureContentContainer">
+
+                                {/* Replace with icon provided from the api */}
+                                <img src={windyIcon} alt="pressure icon" className="w-8 pressureIcon" />
+
+                                {/* Replace with the pressure provided from the api */}
+                                <p className="text-xl font-medium pressure">1019<span className="text-[20px] pressureUnit">hPa</span></p>
+                            </div>
                         </div>
                     </div>
+                    
+                    <div className="flex w-[100%] flex-col md:flex-row md:gap-4 visibilityAndFeelsLikeContainer">
+                        <div className="bg-slate-300 rounded-xl pr-4 pl-4 pt-4 pb-4 mb-4 w-[100%] visibilityContainer">
 
-                    <div className="bg-slate-300 rounded-xl pr-8 pl-8 pt-4 pb-4 mb-4 pressureContainer">
+                            <h3 className="text-[20px] opacity-80 mb-6 visibilityHeader">Visibility</h3>
 
-                        <h3 className="text-[20px] opacity-80 mb-6 pressureHeader">Pressure</h3>
+                            <div className="flex w-[100%] justify-between visibilityContentContainer">
 
-                        <div className="flex w-[100%] justify-between pressureContentContainer">
+                                {/* Replace with icon provided from the api */}
+                                <img src={windyIcon} alt="visibility icon" className="w-8 visibilityIcon" />
 
-                            {/* Replace with icon provided from the api */}
-                            <img src={windyIcon} alt="pressure icon" className="w-8 pressureIcon" />
-
-                            {/* Replace with the pressure provided from the api */}
-                            <p className="text-xl font-medium pressure">1019<span className="text-[20px] pressureUnit">hPa</span></p>
+                                {/* Replace with the visibility provided from the api */}
+                                <p className="text-xl font-medium visibility">2.5<span className="text-[20px] visibilityUnit">km</span></p>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="bg-slate-300 rounded-xl pr-8 pl-8 pt-4 pb-4 mb-4 visibilityContainer">
+                        <div className="bg-slate-300 rounded-xl pr-4 pl-4 pt-4 pb-4 mb-4 w-[100%] feelsLikeContainer">
 
-                        <h3 className="text-[20px] opacity-80 mb-6 visibilityHeader">Visibility</h3>
+                            <h3 className="text-[20px] opacity-80 mb-6 feelsLinkHeader">Feels Like</h3>
 
-                        <div className="flex w-[100%] justify-between visibilityContentContainer">
+                            <div className="flex w-[100%] justify-between feelsLikeContentContainer">
 
-                            {/* Replace with icon provided from the api */}
-                            <img src={windyIcon} alt="visibility icon" className="w-8 visibilityIcon" />
+                                {/* Replace with icon provided from the api */}
+                                <img src={windyIcon} alt="feelsLike icon" className="w-8 feelsLikeIcon" />
 
-                            {/* Replace with the visibility provided from the api */}
-                            <p className="text-xl font-medium visibility">2.5<span className="text-[20px] visibilityUnit">km</span></p>
-                        </div>
-                    </div>
-
-                    <div className="bg-slate-300 rounded-xl pr-8 pl-8 pt-4 pb-4 mb-4 feelsLikeContainer">
-
-                        <h3 className="text-[20px] opacity-80 mb-6 feelsLinkHeader">Feels Like</h3>
-
-                        <div className="flex w-[100%] justify-between feelsLikeContentContainer">
-
-                            {/* Replace with icon provided from the api */}
-                            <img src={windyIcon} alt="feelsLike icon" className="w-8 feelsLikeIcon" />
-
-                            {/* Replace with the feelsLike provided from the api */}
-                            <p className="text-xl font-medium relative feelsLike ">20<span className="text-[20px] absolute top-0 feelsLikeUnit">°c</span></p>
+                                {/* Replace with the feelsLike provided from the api */}
+                                <p className="text-xl font-medium relative feelsLike ">20<span className="text-[20px] absolute top-0 feelsLikeUnit">°c</span></p>
+                            </div>
                         </div>
                     </div>
 
                 </div>
 
-                <div className="mb-8 todayTimesContainer">
+                <div className="mb-8 mt-10 todayTimesContainer">
                     <h2 className="text-xl font-medium mb-4  todayTimesHeader">Today at</h2>
 
                     <div className="overflow-x-auto w-[100%] grid grid-cols-[repeat(8,minmax(110px,1fr))] gap-4 todayTimesContentContainer">
@@ -216,12 +253,13 @@ const Body = () => {
                             </div>
                         ))}
                     </div>
+                    <footer>
+                        <p className="text-[20px] mt-8 text-center">Copyright 2024 Naing Lu Tun, All Rights Reserved</p>
+                     </footer>
                 </div>
             </div>
 
-            <footer>
-                <p className="text-[20px] text-center">Copyright 2024 Naing Lu Tun, All Rights Reserved</p>
-            </footer>
+            
         </div>
     </>
 
