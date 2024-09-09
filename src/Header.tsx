84e879f38d51b3507de58ebfa7ca9ec43aa7cebc
Naing Lu Tun <nainglu7924@gmail.com>
2024-09-09 22:09:08 +0630

    successfully implemented responsive design for all screens

--- src/Header.tsx
+++ src/Header.tsx
@@ -21,13 +21,38 @@ const Header = () => {
             <img src={appIcon} alt="flowy icon" className="w-[30px] md:w-[40px] hover:cursor-pointer appIcon" />
             <h1 className="text-xl font-semibold hover:cursor-pointer headerText">Flowy</h1>
         </div>
-        <div className="gap-3 align-middle flex ">
+        
+
+            {isBigScreen? 
+            
+            <div className="gap-3 align-middle flex">
+
+              <div className="relative searchBarContainer">
+                <input type="text" className="bg-slate-200 p-2 pl-3 pr-[40px] w-[200px] rounded-full focus:outline-none " placeholder="Search location... "/>
+                <img onClick={() => setSearchActive(true)} src={searchIcon} alt="search icon" className="w-[25px] md:w-[35px] absolute right-[2px] top-[2px] bg-blue-400 rounded-full p-1 hover:cursor-pointer searchIcon"/>
+              </div>
+
+              <div className="p-2 pl-3 font-medium text-white relative flex w-[200px] bg-[#cebdf3] rounded-full hover:cursor-pointer currentLocationContainer">
+                <p>Current location</p>
+                <img src={currentLocationIcon} alt="current location icon" className="w-[25px] md:w-[35px] absolute right-[2px] top-[2px] bg-[#b7a0e7] rounded-full hover:cursor-pointer currentLocationIcon" />
+              </div>
+
+              <img onClick={() => setIsDarkMode(!isDarkmode)} src={isDarkmode? lightModeIcon : darkModeIcon} alt="theme icon" className="w-[25px] md:w-[35px] hover:cursor-pointer themeIcon"/>
+
+            </div>
+            
+            : 
+            
+            <div className="gap-3 align-middle flex ">
             <img onClick={() => setSearchActive(true)} src={searchIcon} alt="search icon" className="w-[30px] md:w-[40px] bg-blue-400 rounded-full p-1 hover:cursor-pointer searchIcon"/>
 
             <img src={currentLocationIcon} alt="current location icon" className="w-[30px] md:w-[40px] bg-[#b7a0e7] rounded-full hover:cursor-pointer currentLocationIcon" />
 
             <img onClick={() => setIsDarkMode(!isDarkmode)} src={isDarkmode? lightModeIcon : darkModeIcon} alt="theme icon" className="w-[30px] md:w-[40px] hover:cursor-pointer themeIcon"/>
-        </div>
+            </div>
+            }
+            
+        
 
         {searchActive && <SearchModal setSearchActive={setSearchActive}/>}
 
